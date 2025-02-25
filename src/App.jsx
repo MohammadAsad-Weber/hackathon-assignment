c2e40907c2e368deba0f97d0ce5da6a9f5a53704
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-02-25 19:37:19 +0530

    Assembled the components into the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+
+// Components
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Universities from "./components/Universities";
+import Benefits from "./components/Benefits";
+import Logos from "./components/Logos";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <header className="relative">
+        <Header />
+        <Navbar />
+      </header>
+      <main>
+        <Hero />
+        <Universities />
+        <Benefits />
+        <Logos />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
