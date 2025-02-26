0b3388e3586615cb9e715310ab7046343f4c807f
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-02-26 12:56:44 +0530

    Responive Universities Component

--- src/components/Universities.jsx
+++ src/components/Universities.jsx
@@ -3,12 +3,12 @@ import UniversitiesImages from "../assets/universities.png";
 
 function Universities() {
   return (
-    <section className="h-40 overflow-y-hidden p-5 flex bg-[#D9D9D9]">
-      <img
-        src={UniversitiesImages}
-        alt="Universities Images"
-        className="w-full h-full object-cover object-top"
-      />
+    <section className="h-fit w-full overflow-y-hidden p-2.5 flex bg-[#D9D9D9]">
+        <img
+          src={UniversitiesImages}
+          alt="Universities Images"
+          className="w-full h-full object-contain"
+        />
     </section>
   );
 }
