a01d2e13f960d70c1d5ef98bf5ff23a0f4ca528d
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-02-26 13:33:19 +0530

    Responive Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="h-[20vh] flex items-center justify-center bg-[linear-gradient(300deg,_#31213b_0%,_#110618_100%)]">
-      <div className="w-full flex items-center justify-around">
+    <footer className="h-fit min-h-[20vh] py-10 flex items-center justify-center bg-[linear-gradient(300deg,_#31213b_0%,_#110618_100%)]">
+      <div className="w-full flex flex-col items-center justify-around gap-5 sm:flex-row">
         <h4>
           Copyright <span className="text-gray-500">©</span> 2024 DcodeBlock
         </h4>
-        <div className="flex items-center gap-5">
-          <h3>Help Center</h3>|<h3>Terms and Conditions</h3>|
+        <div className="flex flex-col items-center gap-2.5 lg:flex-row lg:gap-5">
+          <h3>Help Center</h3>
+          <span className="hidden lg:inline">|</span>
+          <h3>Terms and Conditions</h3>
+          <span className="hidden lg:inline">|</span>
           <h3>Privacy policy</h3>
         </div>
       </div>
