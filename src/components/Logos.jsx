5bdca3e8d1af42ab943131088927e7b27cffea03
MohammadAsad-Weber <mohdasad69690@gmail.com>
2025-02-26 13:28:23 +0530

    Responive Logos Component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -10,13 +10,13 @@ import BeyondTheCode from "../assets/beyond-the-code.png";
 
 function Logos() {
   return (
-    <section className="h-25 p-5 flex items-center justify-evenly gap-5 bg-[#D9D9D9]">
-      <img src={SingularityBlack} alt="Singularity Image" className="aspect-auto h-full" />
-      <img src={Rair} alt="Rair Image" className="aspect-auto h-full" />
-      <img src={Stratis} alt="Stratis Image" className="aspect-auto h-full" />
-      <img src={SharpEconomy} alt="Sharp Economy Image" className="aspect-auto h-full" />
-      <img src={CSharp} alt="CSharp.TV Image" className="aspect-auto h-full" />
-      <img src={BeyondTheCode} alt="Beyond The Code Image" className="aspect-auto h-full" />
+    <section className="max-h-25 h-fit p-5 flex items-center justify-evenly gap-5 bg-[#D9D9D9]">
+      <img src={SingularityBlack} alt="Singularity Image" className="aspect-auto w-1/8" />
+      <img src={Rair} alt="Rair Image" className="aspect-auto w-1/8" />
+      <img src={Stratis} alt="Stratis Image" className="aspect-auto w-1/8" />
+      <img src={SharpEconomy} alt="Sharp Economy Image" className="aspect-auto w-1/8" />
+      <img src={CSharp} alt="CSharp.TV Image" className="aspect-auto w-1/8" />
+      <img src={BeyondTheCode} alt="Beyond The Code Image" className="aspect-auto w-1/8" />
     </section>
   );
 }
